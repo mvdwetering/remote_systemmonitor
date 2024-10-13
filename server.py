489981e0f5a657ba8c1b99fbfe4ac517857ef577
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2024-10-13 22:41:50 +0200

    Add initial websocket test

--- server.py
+++ server.py
@@ -8,10 +8,38 @@ import asyncio
 import json
 import logging
 
+from websockets.asyncio.server import broadcast, serve
+
 from server import async_setup_entry
 from server.hass_stubs import DEFAULT_SCAN_INTERVAL, ConfigEntry, HomeAssistant
 
+CONNECTIONS = set()
 
+async def consumer_handler(websocket):
+    async for message in websocket:
+        print(message)
+
+async def producer_handler(websocket):
+    while True:
+        await asyncio.sleep(5)
+        # message = await producer()
+        message = "test"
+        broadcast(CONNECTIONS, message)
+        # await websocket.send(message)
+
+async def websocket_handler(websocket):
+    CONNECTIONS.add(websocket)
+
+    try:
+        # async for message in websocket:
+        #     print(message)        
+        # await websocket.wait_closed()        
+        await asyncio.gather(
+            consumer_handler(websocket),
+            producer_handler(websocket),
+        )
+    finally:
+        CONNECTIONS.remove(websocket)
 
 async def main(args):
     hass = HomeAssistant()
@@ -26,12 +54,15 @@ async def main(args):
     for disk_argument in disk_arguments:
         entry.runtime_data.coordinator.update_subscribers[("disks", disk_argument)] = "dummy"  # Would normally be entity_id
 
+    async with serve(websocket_handler, "localhost", 8765):
+        while True:
+            new_data = await entry.runtime_data.coordinator._async_update_data()
+            new_data = new_data.as_dict()
 
-    while True:
-        new_data = await entry.runtime_data.coordinator._async_update_data()
-        print(json.dumps(new_data.as_dict(), indent=2))
+            print(json.dumps(new_data, indent=2))
+            broadcast(CONNECTIONS, json.dumps(new_data))
 
-        await asyncio.sleep(DEFAULT_SCAN_INTERVAL)
+            await asyncio.sleep(DEFAULT_SCAN_INTERVAL)
 
 
 if __name__ == "__main__":
