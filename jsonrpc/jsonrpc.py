0a400da1299be59a93faa36a4a59fb690e8f5e25
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2024-10-26 20:03:00 +0200

    More jsonrpc cleanup

--- jsonrpc/jsonrpc.py
+++ jsonrpc/jsonrpc.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-"""Server API for Remote System Monitor."""
+"""JSON-RPC 2.0 implementation."""
 
 from __future__ import annotations
 
 import asyncio
-from dataclasses import dataclass
 import enum
 import json
 import logging
 from typing import Any, Callable, Coroutine
 import uuid
-
-from mashumaro.mixins.dict import DataClassDictMixin
 
 
 @enum.unique
@@ -24,11 +21,9 @@ class JsonRpcErrorCode(enum.Enum):
 
 
 class JsonRpcResponseError:
-    def __init__(
-        self, code: JsonRpcErrorCode, data=None
-    ) -> None:
+    def __init__(self, code: JsonRpcErrorCode, data=None) -> None:
         self.code = code
-        self.data = None
+        self.data = data
 
     def __str__(self) -> str:
         return json.dumps(self.to_dict())
@@ -44,7 +39,7 @@ class JsonRpcResponse:
     def __init__(
         self,
         id: str | int | float | None,
-        result:Any=None,
+        result: Any = None,
         error: JsonRpcResponseError | None = None,
     ) -> None:
         self.id = id  # id can only be None/Null when id could not be retrieved
@@ -66,6 +61,47 @@ class JsonRpcResponse:
         return message
 
 
+class JsonRpcNotification:
+    def __init__(
+        self,
+        method: str,
+        params: list | dict | None = None,
+    ) -> None:
+        self.method = method
+        self.params = params
+
+    def __str__(self) -> str:
+        return json.dumps(self.to_dict())
+
+    def to_dict(self):
+        message = {
+            "jsonrpc": "2.0",
+            "method": self.method,
+        }
+        if self.params is not None:
+            message["params"] = self.params
+        return message
+
+
+class JsonRpcRequest(JsonRpcNotification):
+    def __init__(
+        self,
+        method: str,
+        params: list | dict | None = None,
+    ) -> None:
+        self._id = uuid.uuid4().hex
+        super().__init__(method, params)
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    def to_dict(self):
+        message = super().to_dict()
+        message["id"] = self._id
+        return message
+
+
 class JsonRpc:
     def __init__(self, backend) -> None:
         self._backend = backend
@@ -79,10 +115,6 @@ class JsonRpc:
         self._notification_handlers[method] = handler
 
     def register_request_handler(self, method, handler):
-        """
-        Register a request handler for a method.
-        Note that this is UNTESTED !!!
-        """
         self._request_handlers[method] = handler
 
     async def call_method(
@@ -90,155 +122,146 @@ class JsonRpc:
     ) -> JsonRpcResponse:
         logging.debug("Call method: %s, params: %s", method, params)
 
-        id = uuid.uuid4().hex
-        message = {
-            "jsonrpc": "2.0",
-            "id": id,
-            "method": method,
-        }
-        if params:
-            message["params"] = params
+        pending_future: asyncio.Future = asyncio.Future()
+
+        request = JsonRpcRequest(method, params)
+        self._pending_method_calls[request.id] = pending_future
+
+        await self._backend.send(str(request))
 
         # TODO: Add some kind of timeout?
-        pending_future: asyncio.Future = asyncio.Future()
-        self._pending_method_calls[id] = pending_future
-
-        await self._backend.send(json.dumps(message))
-
         await pending_future
         return pending_future.result()
 
     async def send_notification(self, method: str, params: Any | None = None) -> None:
-        logging.debug("Call method: %s, params: %s", method, params)
+        logging.debug("Send notification, method: %s, params: %s", method, params)
+        await self._backend.send(str(JsonRpcNotification(method, params)))
 
-        message = {
-            "jsonrpc": "2.0",
-            "method": method,
-        }
-        if params:
-            message["params"] = params
+    async def _handle_notification(self, method, params):
+        notification_handler = self._notification_handlers.get(method, None)
+        if notification_handler is None:
+            logging.debug("No notification handler for method: %s", method)
+            return None
 
-        await self._backend.send(json.dumps(message))
+        try:
+            if isinstance(params, list):
+                await notification_handler(*params)
+            elif isinstance(params, dict):
+                await notification_handler(**params)
+            elif params is None:
+                await notification_handler()
+        except Exception:
+            logging.exception("Exception in notification handler")
+
+        # No responses for notifications
+        return None
+
+    async def _handle_request(self, id, method, params):
+        request_handler = self._request_handlers.get(method, None)
+        if request_handler is None:
+            logging.debug("No request handler for method: %s", method)
+            return str(
+                JsonRpcResponse(
+                    id=id,
+                    error=JsonRpcResponseError(JsonRpcErrorCode.METHOD_NOT_FOUND),
+                )
+            )
+
+        # TODO: Maybe requests should be handled an a separate task?
+        result = None
+        error = None
+        try:
+            if isinstance(params, list):
+                result = await request_handler(*params)
+            elif isinstance(params, dict):
+                result = await request_handler(**params)
+            elif params is None:
+                result = await request_handler()
+            else:
+                error = JsonRpcResponseError(JsonRpcErrorCode.INVALID_PARAMS)
+        except Exception:
+            error = JsonRpcResponseError(JsonRpcErrorCode.INTERNAL_ERROR)
+
+        return str(JsonRpcResponse(id, result=result, error=error))
+
+    async def _handle_response(self, id, result, error):
+        if pending_method_handler := self._pending_method_calls.pop(id, None):
+            pending_method_handler.set_result(JsonRpcResponse(id, result, error))
+            return None
+
+        logging.warning("No pending response handler for id: %s", id)
+        return None
 
     async def _on_receive(self, inbound_message: str) -> str | None:
         logging.debug("On receive, message %s", inbound_message)
 
         try:
             message = json.loads(inbound_message)
+        except json.JSONDecodeError:
+            logging.warning(f"Invalid JSON message: {inbound_message}")
 
-            if isinstance(message, list):
-                logging.error(
-                    "BATCH request objects not supported yet. Message is ignored"
+            return str(
+                JsonRpcResponse(
+                    id=None, error=JsonRpcResponseError(JsonRpcErrorCode.PARSE_ERROR)
                 )
-                return None
-
-            if message.get("jsonrpc", None) != "2.0":
-                return str(
-                    JsonRpcResponse(
-                        id=None,
-                        error=JsonRpcResponseError(JsonRpcErrorCode.INVALID_REQUEST),
-                    )
-                )
-
-            method = message.get("method", None)
-            if method and not isinstance(method, str):
-                return str(
-                    JsonRpcResponse(
-                        id=None,
-                        error=JsonRpcResponseError(JsonRpcErrorCode.INVALID_REQUEST),
-                    )
-                )
-            params = message.get(
-                "params", None
-            )  # TODO: Add more checking, params should be a dict or list
-            id = message.get("id", None)
-            result = message.get("result", None)
-            error = message.get("error", None)
-
-            if method and id is None:
-                logging.debug("Notification message received for method: %s", method)
-                notification_handler = self._notification_handlers.get(
-                    method, None
-                )
-
-                if notification_handler is None:
-                    logging.debug("No notification handler for method: %s", method)
-                    return None
-
-                try:
-                    if isinstance(params, list):
-                        await notification_handler(*params)
-                    elif isinstance(params, dict):
-                        await notification_handler(**params)
-                    elif params is None:
-                        await notification_handler()
-                except Exception:
-                    logging.exception("Exception in notification handler")
-
-                # No responses for notifications
-                return None
-
-
-            if method:
-                logging.debug("Request message received for method: %s", method)
-                if request_handler := self._request_handlers.get(method, None):
-                    # TODO: Maybe requests should be handled an a separate task?
-                    try:
-                        if isinstance(params, list):
-                            result = await request_handler(*params)
-                        elif isinstance(params, dict):
-                            result = await request_handler(**params)
-                        elif params is None:
-                            result = await request_handler()
-                        else:
-                            error = JsonRpcResponseError(JsonRpcErrorCode.INVALID_PARAMS)
-                    except Exception:
-                        error = JsonRpcResponseError(JsonRpcErrorCode.INTERNAL_ERROR)
-
-                    message = {
-                        "jsonrpc": "2.0",
-                        "id": id,
-                    }
-
-                    return str(JsonRpcResponse(id, result=result, error=error))
-                    # if response.result is not None:
-                    #     message["result"] = response.result
-                    # if response.error is not None:
-                    #     message["error"] = response.error.to_dict()                    
-                    return message
-
-                logging.debug("No request handler for method: %s", message["method"])
-                return str(
-                    JsonRpcResponse(
-                        id=id,
-                        error=JsonRpcResponseError(JsonRpcErrorCode.METHOD_NOT_FOUND),
-                    )
-                )
-
-            if id and (result or error):
-                logging.debug("Response message received for id: %s, result: %s, error: %s", id, result, error)
-                if pending_method_handler := self._pending_method_calls.pop(id, None):
-                    pending_method_handler.set_result(JsonRpcResponse(id, result, error))
-                    return None
-                else:
-                    logging.warning("No pending response handler for id: %s", id)
-                    return None
-
-            logging.warning(
-                "Invalid JSON-RPC message. Not a request, notification or response... : %s",
-                inbound_message,
             )
+
+        if isinstance(message, list):
+            logging.warning(
+                "BATCH request objects not supported yet. Message is ignored"
+            )
+            return None
+
+        if message.get("jsonrpc", None) != "2.0":
             return str(
                 JsonRpcResponse(
                     id=None,
                     error=JsonRpcResponseError(JsonRpcErrorCode.INVALID_REQUEST),
                 )
             )
-        except json.JSONDecodeError:
-            logging.warning(f"Invalid JSON message: {inbound_message}")
 
+        method = message.get("method", None)
+        if method and not isinstance(method, str):
+            return str(
+                JsonRpcResponse(
+                    id=None,
+                    error=JsonRpcResponseError(JsonRpcErrorCode.INVALID_REQUEST),
+                )
+            )
+        params = message.get(
+            "params", None
+        )  # TODO: Add more checking, params should be a dict or list
+        id = message.get("id", None)
+        result = message.get("result", None)
+        error = message.get("error", None)
+
+        if method and id is None:
+            logging.debug("Notification message received for method: %s", method)
+            response = await self._handle_notification(method, params)
+            return response
+
+        if method:
+            logging.debug("Request message received for method: %s", method)
+            response = await self._handle_request(id, method, params)
+            return response
+
+        if id and (result or error):
+            logging.debug(
+                "Response message received for id: %s, result: %s, error: %s",
+                id,
+                result,
+                error,
+            )
+            response = await self._handle_response(id, result=result, error=error)
+            return response
+
+        logging.warning(
+            "Invalid JSON-RPC message. Not a request, notification or response... : %s",
+            inbound_message,
+        )
         return str(
             JsonRpcResponse(
-                id=None, error=JsonRpcResponseError(JsonRpcErrorCode.PARSE_ERROR)
+                id=None,
+                error=JsonRpcResponseError(JsonRpcErrorCode.INVALID_REQUEST),
             )
         )
